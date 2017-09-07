e15bee26c97e2409856a65d89f060c61ed39cba1
Pekka Heino <pxi@usr.fi>
2017-09-08 00:16:30 +0300

    Initial implementation

---
+++ is.go
@@ -0,0 +1,23 @@
+// Package is provides helpers for testing.
+package is
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Ok fails the test if the given error is not nil.
+func Ok(tb testing.TB, err error) {
+	tb.Helper()
+	if err != nil {
+		tb.Fatal(err)
+	}
+}
+
+// Equal fails the test if got is not equal to want.
+func Equal(tb testing.TB, got, want interface{}) {
+	tb.Helper()
+	if !reflect.DeepEqual(got, want) {
+		tb.Fatalf("\n got: %#v\nwant: %#v", got, want)
+	}
+}
